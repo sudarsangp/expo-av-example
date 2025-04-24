6a96e969da31abffd2acab496ba1766819d98645
Sudarsan <sudarsangp@gmail.com>
2025-04-24 12:28:42 +0530

    feat: minimum reproducible example

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {Audio} from 'expo-av';
 
 export default function App() {
+
+  const playAudio = async () => {
+    const { sound } = await Audio.Sound.createAsync(        
+      {
+        uri: 'https://sudarsangp.com/expo-audio-test.mp3'
+      }
+    );
+    await sound.playAsync();
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title="Click to Play audio" onPress={playAudio}></Button>
     </View>
   );
 }
